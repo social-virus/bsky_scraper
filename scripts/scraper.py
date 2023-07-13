581cc5505f89f03e61431d4eee8c006d3d24aec3
Social <127993470+social-virus@users.noreply.github.com>
2023-07-13 18:04:34 -0500

    Suppress .

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -10,7 +10,7 @@ from atproto.exceptions import UnauthorizedError
 
 import context  # pylint: disable=unused-import
 
-# pylint: disable=no-name-in-module
+# pylint: disable=no-name-in-module,import-error
 from lib.arguments import parse_args
 from lib.skyscraper import BskyClient
 
