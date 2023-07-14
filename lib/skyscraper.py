ab24330d3edd049bd1c4c79cb01fbf86cf909d85
Social <127993470+social-virus@users.noreply.github.com>
2023-07-13 22:59:58 -0500

    Add get_profile method.

--- lib/skyscraper.py
+++ lib/skyscraper.py
@@ -5,10 +5,13 @@ Bluesky Scraper library.
 import time
 
 from dataclasses import asdict, fields
-from typing import Callable, Dict
+from typing import Callable, Dict, List
 from random import uniform
 
 from atproto import Client
+from atproto.xrpc_client.models.app.bsky.actor.defs import ProfileViewDetailed
+
+from .utils import fetch_image
 
 
 BSKY_SUFFIX = ".bsky.social"
@@ -70,3 +73,11 @@ class BskyClient:
         graph = self.client.bsky.graph
 
         return self.looping_caller(graph.get_follows, params)
+    
+    def get_profile(self, actor: Union[str, List[str]]) -> ProfileViewDetailed:
+        if isinstance(actor, List):
+            actor = [append_bsky_domain(act) for act in actor]
+            return self.bsky.actor.get_profiles(actors=actor)
+        
+        actor = append_bsky_domain(actor)
+        return self.bsky.actor.get_profile(actor=actor)
